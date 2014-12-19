9a81a0d77afeb78cabc2275933e698fcb8f54e1d
Dugan Chen <dugan-c@moving-picture.com>
2014-12-19 11:20:12 -0800

    XML is much more well formed

--- indexer/indexer_site_extensions.py
+++ indexer/indexer_site_extensions.py
@@ -1,3 +1,14 @@
+def is_indexable_file(abspath):
+    '''
+    Given the absolute path to a git blob in a repository, returns whether to
+    index that file.
+
+    The default implementation always returns True.
+    '''
+
+    return not abspath.endswith('min.js')
+
+
 def is_indexable_line(line):
     '''
     Given a line, returns whether to index that line. Override this if you want
